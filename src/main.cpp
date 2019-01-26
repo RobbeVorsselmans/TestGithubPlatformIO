bc4623603c9595f61c93205b83705f75c6483dc4
Robbe Vorsselmans <robbevorsselmans@gmail.com>
2019-01-26 21:42:56 +0100

    rampBlink

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,29 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
-}
+#define ledpin 13
+int lowerBound = 50;
+int upperBound = 1000;
+bool rampUp = true;
+int delayTime = lowerBound;
+
+void setup() { pinMode(ledpin, OUTPUT); }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(ledpin, HIGH);
+  delay(delayTime);
+  digitalWrite(ledpin, LOW);
+  delay(delayTime);
+  if (rampUp) {
+    delayTime *= 2;
+    Serial.print("delayTime"); Serial.print(" : "); Serial.println(delayTime);
+    if (delayTime > upperBound) {
+      rampUp = false;
+    }
+  } else {
+    delayTime /= 2;
+    Serial.print("delayTime"); Serial.print(" : "); Serial.println(delayTime);
+    if (delayTime < lowerBound) {
+      rampUp = true;
+    }
+  }
 }
